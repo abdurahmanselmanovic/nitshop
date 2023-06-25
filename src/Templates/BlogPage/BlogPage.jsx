35e96ed575d82f5ceadb9b6d3cb5f549423033eb
abdurrahman <selmanovicabdurrahman@gmail.com>
2023-06-26 00:23:21 +0200

    read me md

--- src/Templates/BlogPage/BlogPage.jsx
+++ src/Templates/BlogPage/BlogPage.jsx
@@ -1,43 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { fetchComments } from "../../api/commentsApi";
+import CategoryTab from "../CategoryTab/CategoryTab";
+import { Button, Grid, TextField } from "@mui/material";
+import SimplifiedDiv from "../../components/SimplifiedDiv/SimplifiedDiv";
 
 const BlogPage = () => {
-  const [num, setNum] = useState([]);
-  const [chosenNum, setChosenNum] = useState();
-  const [comments, setComments] = useState([]);
-
-  useEffect(() => {
-    if (num.length === 10) return;
-    for (let i = 1; i <= 10; i++) {
-      setNum((prev) => [...prev, i]);
-    }
-  }, []);
-
-  useEffect(() => {
-    fetchComments().then((values) => {
-      console.log(values);
-      let filtered = values.filter((val) => val.postId == chosenNum);
-      setComments(filtered);
-    });
-  }, [chosenNum]);
-
-  function submitHandler(val) {
-    console.log(val.target.value);
-    setChosenNum(val.target.value);
-  }
-
-  console.log(comments);
-
-  return (
-    <>
-      <select onChange={submitHandler}>
-        {num.map((n) => (
-          <option value={n}>{n}</option>
-        ))}
-      </select>
-      <div>{comments.toString()}</div>
-    </>
-  );
+	return (
+		<Grid container spacing={2}>
+			<Grid item xs={3}>
+				<h1>Category</h1>
+				<SimplifiedDiv>
+					<CategoryTab />
+				</SimplifiedDiv>
+			</Grid>
+			<Grid item xs={9}></Grid>
+		</Grid>
+	);
 };
 
 export default BlogPage;
