efdabf64c39b068fd99211bdfdac6b051db25b4a
abdurrahman <selmanovicabdurrahman@gmail.com>
2023-07-01 00:14:46 +0200

    added styles on checkout

--- src/Templates/Checkout/Checkout.jsx
+++ src/Templates/Checkout/Checkout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import SimplifiedDiv from "../../components/SimplifiedDiv/SimplifiedDiv";
 import HighlightOffRoundedIcon from "@mui/icons-material/HighlightOffRounded";
-import { Checkbox } from "@mui/material";
+import { Button, Checkbox, Grid, TextField } from "@mui/material";
 const Checkout = () => {
 	const styles = {
 		parentDiv: {
@@ -18,7 +18,7 @@ const Checkout = () => {
 		secondtDivSpan: {
 			color: "gray",
 			fontWeight: "600",
-			margin: "0 10px 0 0",
+			margin: "0 20px 0 0",
 		},
 		iconX: {
 			color: "orange",
@@ -27,6 +27,31 @@ const Checkout = () => {
 		thirdDivP: {
 			color: "gray",
 			fontSize: "14px",
+			padding: "12px",
+		},
+		inputs: {
+			margin: "4px",
+			width: "90%",
+			heihgt: "100px",
+		},
+		buttons: {
+			backgroundColor: "orange",
+			color: "white",
+			width: "120px",
+			marginTop: "10px",
+			marginLeft: "8px",
+			height: "40px",
+			fontSize: "12px",
+			border: "none",
+		},
+		spanOrange: {
+			color: "orange",
+			fontWeight: "bold",
+			fontSize: "16px",
+			margin: "-40px 0 0 0",
+		},
+		textBox: {
+			backgroundColor: "lightgray",
 			padding: "10px",
 		},
 	};
@@ -45,13 +70,179 @@ const Checkout = () => {
 				<Checkbox></Checkbox>
 				<span style={styles.secondtDivSpan}>Guest Checkout</span>
 				<HighlightOffRoundedIcon style={styles.iconX} />
-				<span> Cancel</span>
+				<span style={styles.spanOrange}> Cancel</span>
 			</SimplifiedDiv>
 			<SimplifiedDiv style={styles.firstDiv}>
 				<p style={styles.thirdDivP}>
 					Please use Register And Checkout to easily get access to your order
 					history, or use Checkout as Guest
 				</p>
+			</SimplifiedDiv>
+			<SimplifiedDiv>
+				<Grid container spacing={2}>
+					<Grid item xs={3.5}>
+						<h3>Shopper information</h3>
+						<TextField
+							style={styles.inputs}
+							size="small"
+							id="filled"
+							label="Display Name"
+							variant="filled"
+						/>
+						<TextField
+							style={styles.inputs}
+							size="small"
+							id="filled"
+							label="User Name"
+							variant="filled"
+						/>
+						<TextField
+							style={styles.inputs}
+							size="small"
+							id="filled"
+							label="Password"
+							variant="filled"
+						/>
+						<TextField
+							style={styles.inputs}
+							size="small"
+							id="filled"
+							label="Confirm Password  "
+							variant="filled"
+						/>
+						<Button style={styles.buttons} variant="contained">
+							Get Quotes
+						</Button>
+						<Button style={styles.buttons} variant="contained">
+							Continue
+						</Button>
+					</Grid>
+					<Grid item xs={5}>
+						<h3>Bill to</h3>
+						<Grid container spacing={2}>
+							<Grid item xs={6}>
+								<TextField
+									size="small"
+									style={styles.inputs}
+									id="filled"
+									label="Company Name"
+									variant="filled"
+								/>
+								<TextField
+									size="small"
+									style={styles.inputs}
+									id="filled"
+									label="Email"
+									variant="filled"
+								/>
+								<TextField
+									size="small"
+									style={styles.inputs}
+									id="filled"
+									label="Title"
+									variant="filled"
+								/>
+								<TextField
+									size="small"
+									style={styles.inputs}
+									id="filled"
+									label="First Name *"
+									variant="filled"
+								/>
+								<TextField
+									size="small"
+									style={styles.inputs}
+									id="filled"
+									label="Middle Name"
+									variant="filled"
+								/>
+								<TextField
+									size="small"
+									style={styles.inputs}
+									id="filled"
+									label="Last Name * "
+									variant="filled"
+								/>
+								<TextField
+									size="small"
+									style={styles.inputs}
+									id="filled"
+									label="Address 1 *"
+									variant="filled"
+								/>
+								<TextField
+									size="small"
+									style={styles.inputs}
+									id="filled"
+									label="Address 2"
+									variant="filled"
+								/>
+							</Grid>
+							<Grid item xs={6}>
+								<TextField
+									size="small"
+									style={styles.inputs}
+									id="filled"
+									label="Zip / Postal code *"
+									variant="filled"
+								/>
+								<TextField
+									size="small"
+									style={styles.inputs}
+									id="filled"
+									label="Country"
+									variant="filled"
+								/>
+								<TextField
+									size="small"
+									style={styles.inputs}
+									id="filled"
+									label="State"
+									variant="filled"
+								/>
+								<TextField
+									style={styles.inputs}
+									size="small"
+									id="filled"
+									label="Confirm Password"
+									variant="filled"
+								/>
+								<TextField
+									size="small"
+									style={styles.inputs}
+									id="filled"
+									label="Phone *"
+									variant="filled"
+								/>
+								<TextField
+									size="small"
+									style={styles.inputs}
+									id="filled"
+									label="Mobile Phone"
+									variant="filled"
+								/>
+								<TextField
+									size="small"
+									style={styles.inputs}
+									id="filled"
+									label="Fax"
+									variant="filled"
+								/>
+							</Grid>
+						</Grid>
+					</Grid>
+					<Grid item xs={3.5}>
+						<h3>Shipping Order</h3>
+						<textarea
+							style={styles.textBox}
+							rows="20"
+							cols="50"
+							placeholder="Notes about your order, Special Notes for Delivery"
+						></textarea>
+						<Checkbox></Checkbox>
+						<span>Shipping to bill address</span>
+					</Grid>
+				</Grid>
 			</SimplifiedDiv>
 		</SimplifiedDiv>
 	);
