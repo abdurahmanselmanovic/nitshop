d17c8158da9b64a623a210c14dcc515885318c50
abdurrahman <selmanovicabdurrahman@gmail.com>
2023-07-02 19:41:18 +0200

    shop page

--- src/Screens/ShopScreen/ShopScreen.jsx
+++ src/Screens/ShopScreen/ShopScreen.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import Navigation from "../../Templates/Navigation/Navigation";
 import Footer from "../../Templates/Footer/Footer";
 import CounterComponent from "../../Templates/CounterComponent/CounterComponent";
+import ShopPage from "../../Templates/ShopPage/ShopPage";
 
 const ShopScreen = () => {
 	return (
 		<>
 			<Navigation />
-			<CounterComponent />
+			<ShopPage />
 			<Footer />
 		</>
 	);
--- src/Templates/ShopPage/ShopPage.jsx
+++ src/Templates/ShopPage/ShopPage.jsx
@@ -1,7 +1,56 @@
 import React from "react";
+import SimplifiedDiv from "../../components/SimplifiedDiv/SimplifiedDiv";
+import Banner from "../../assets/images/shop/advertisement.jpg";
+import { Grid } from "@mui/material";
+import CategoryTab from "../CategoryTab/CategoryTab";
+import freeshipping from "../../assets/images/home/shipping.jpg";
+import ArticleCard from "../ArticleCard/ArticleCard";
+import ArticlesHomePage from "../ArticlesHomePage/ArticlesHomePage";
 
 const ShopPage = () => {
-	return <div>ShopPage</div>;
+	const styles = {
+		glavniDiv: {
+			marginTop: "40px",
+			marginBottom: "40px",
+			padding: "0 10%",
+		},
+		imgBanner: {
+			marginBottom: "15px",
+		},
+		grid1H1: {
+			color: "orange",
+			textAlign: "center",
+			fontWeight: "bold",
+			fontSize: "18px",
+		},
+		grid1img2: {
+			marginTop: "100px",
+			width: "100%",
+		},
+	};
+	return (
+		<>
+			<SimplifiedDiv style={styles.glavniDiv}>
+				<img
+					style={styles.imgBanner}
+					src={Banner}
+					alt=""
+					srcset=""
+					width={"100%"}
+				/>
+				<Grid container spacing={2}>
+					<Grid style={styles.grid1} item xs={3}>
+						<h1 style={styles.grid1H1}>CATEGORY</h1>
+						<CategoryTab />
+						<img style={styles.grid1img2} src={freeshipping} alt="s"></img>
+					</Grid>
+					<Grid item xs={9}>
+						<ArticlesHomePage />
+					</Grid>
+				</Grid>
+			</SimplifiedDiv>
+		</>
+	);
 };
 
 export default ShopPage;
