55aa2a8b4b49014d67c63a1c70d7cb53b165781f
abdurrahman <selmanovicabdurrahman@gmail.com>
2023-07-02 19:18:56 +0200

    shop page

--- src/Screens/ShopScreen/ShopScreen.jsx
+++ src/Screens/ShopScreen/ShopScreen.jsx
@@ -1,18 +1,16 @@
-import React, { useContext } from "react";
+import React from "react";
 import Navigation from "../../Templates/Navigation/Navigation";
 import Footer from "../../Templates/Footer/Footer";
-import { CartContext } from "../../context/CartContext";
-import { UserContext } from "../../context/UserContext";
 import CounterComponent from "../../Templates/CounterComponent/CounterComponent";
 
 const ShopScreen = () => {
-  return (
-    <>
-      <Navigation />
-      <CounterComponent />
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Navigation />
+			<CounterComponent />
+			<Footer />
+		</>
+	);
 };
 
 export default ShopScreen;
